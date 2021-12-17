059d31bd57f7646d820668101a9f1d9e7a267ee2
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-17 11:13:28 -0800

    added getTeams function

--- src/services/teams.js
+++ src/services/teams.js
@@ -1,1 +1,7 @@
 // fetch teams and fetch teams by id
+import client, { checkError } from './client';
+
+export async function getTeams() {
+  const resp = await client.from('teams').select().order('name');
+  return checkError(resp);
+}
